660b547e609ead5603495d42fd10db2c499277e6
Sandeep Saini <sandeepsaini157219@gmail.com>
2026-08-27 01:23:52 +0530

    he1211

--- app/services/ContactHeader/ContactHeader.tsx
+++ app/services/ContactHeader/ContactHeader.tsx
@@ -1,1 +1,20 @@
-<section className="text-center pt-28 sm:pt-36 pb-10 sm:pb-20 px-4 bg-gradient-to-br from-white to-gray-300 dark:from-navy dark:to-darkbg2">
+"use client";
+
+const ContactHeader = () => {
+  return (
+    <section className="text-center pt-28 sm:pt-36 pb-10 sm:pb-20 px-4 bg-gradient-to-br from-white to-gray-300 dark:from-navy dark:to-darkbg2">
+      <div>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-navy dark:text-white mb-3 sm:mb-4">
+          Our Services
+        </h1>
+
+        <p className="text-gray-700 dark:text-gray-400 max-w-xl mx-auto text-sm sm:text-base md:text-lg px-2 sm:px-0">
+          From design to deployment to AI automation — we build powerful digital
+          solutions that scale with your vision.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default ContactHeader;
